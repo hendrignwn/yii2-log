1e50db00eb6b19849c8506ea89a1ccdd0b67f474
Hendri <hendri.gnw@gmail.com>
2016-08-08 09:41:11 +0700

    initial commit

---
+++ Module.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace hendrignwn\log;
+
+/**
+ * log-module module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'hendrignwn\log\controllers';
+	
+	public $defaultRoute = 'log/index';
+    
+    public $layout = 'main';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        
+        if(\Yii::$app->user->isGuest) {
+			throw new \yii\web\HttpException(404, 'Page not found..');
+		}
+		
+        // custom initialization code goes here
+    }
+}
